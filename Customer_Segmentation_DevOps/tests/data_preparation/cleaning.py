a02da76b3f410457939a5f0d9e0c25d388114306
rawbeen248 <rawbeendraw.248@gmail.com>
2023-11-09 17:28:28 +0100

    Initialixing tests

--- Customer_Segmentation_DevOps/tests/data_preparation/cleaning.py
+++ Customer_Segmentation_DevOps/tests/data_preparation/cleaning.py
@@ -1,56 +1,55 @@
+import unittest
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-import json 
-import logging
+import json  
+import os
+import sys
 
-# Set up logging configuration
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# Get the current script's directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-def prepare_data(filepath):
-    """
-    Load data from a CSV file, remove duplicates, and drop specific columns.
-    """
-    logging.info('Preparing data...')
-    
-    try:
-        df = pd.read_csv(filepath)
-    except FileNotFoundError:
-        logging.error("File not found: {}".format(filepath))
-        return
-    
-    df.drop_duplicates(inplace=True)
-    cat =  ['product_name_lenght','product_description_lenght','shipping_limit_date','product_category_name']
-    df.drop(columns=cat, axis=1, inplace=True)
-    logging.debug('Data after removing duplicates and dropping columns:\n{}'.format(df.head()))
-    logging.info('Data prepared successfully.')
-    return df
+# Construct the path to the src directory
+src_dir = os.path.join(script_dir, '..', '..', 'src')
 
-def drop_c_id(df):
-    """
-    Drop the customer_unique_id column.
-    """
-    logging.info('Dropping customer id...')
-    logging.debug('Number of unique customer ids before dropping: {}'.format(df['customer_id'].nunique()))
-    
-    if 'customer_unique_id' in df.columns:
-        df.drop(columns='customer_unique_id', inplace=True)
-    else:
-        logging.warning("'customer_unique_id' not found in DataFrame.")
-        
-    logging.debug('Number of unique customer ids after dropping: {}'.format(df['customer_id'].nunique()))
-    logging.debug('Data after dropping customer id:\n{}'.format(df.head()))
-    logging.info('Customer id dropped successfully.')
-    return df 
+# Add the src directory to the module search path
+sys.path.append(src_dir)
+from data_preparation.cleaning import prepare_data, drop_c_id, clean_data
 
-def clean_data(df):
-    """
-    Filter data by order status and sample a fraction.
-    """
-    logging.info('Cleaning data...')
-    
-    df = pd.DataFrame(df)
-    df = df[df['order_status'] == 'delivered']
-    df = df.sample(frac=0.1, random_state=1)
-    logging.debug('Data after filtering by order status and sampling:\n{}'.format(df.head()))
-    logging.info('Data cleaned successfully.')
-    return df
+class TestDataPreparation(unittest.TestCase):
+    def setUp(self):
+        # Create a temporary CSV file for testing
+        self.test_data = pd.DataFrame({
+            'customer_id': [1, 2, 3],
+            'order_status': ['delivered', 'shipped', 'delivered']
+            # Add other necessary columns for your tests
+        })
+        self.temp_file = 'test_data.csv'
+        self.test_data.to_csv(self.temp_file, index=False)
+
+    def tearDown(self):
+        # Remove the temporary CSV file
+        os.remove(self.temp_file)
+
+    def test_prepare_data(self):
+        df = prepare_data(self.temp_file)
+        # Add your assertions based on the expected behavior of prepare_data
+        self.assertTrue('customer_id' in df.columns)
+        self.assertTrue('order_status' in df.columns)
+        # Add more assertions as needed
+
+    def test_drop_c_id(self):
+        df = drop_c_id(self.test_data)
+        # Add your assertions based on the expected behavior of drop_c_id
+        self.assertTrue('customer_id' in df.columns)
+        self.assertFalse('customer_unique_id' in df.columns)
+        # Add more assertions as needed
+
+    def test_clean_data(self):
+        df = clean_data(self.test_data)
+        # Add your assertions based on the expected behavior of clean_data
+        self.assertTrue('customer_id' in df.columns)
+        self.assertTrue('order_status' in df.columns)
+        self.assertEqual(df['order_status'].unique(), ['delivered'])
+        # Add more assertions as needed
+
+if __name__ == '__main__':
+    unittest.main()
